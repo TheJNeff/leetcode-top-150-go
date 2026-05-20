776fa446a8577c4d99737c02b9674823c4c5b733
TheJNeff <62257733+TheJNeff@users.noreply.github.com>
2026-05-19 18:09:13 -0700

    Solve #61: Rotate List (Medium)

--- java/medium/rotate-list/solution.java
+++ java/medium/rotate-list/solution.java
@@ -24,20 +24,30 @@
 
  Not so fast! If you only have the start and the end, how do you point the node before the end to null? You don't know which one it is!
 
- Maybe instead of actually removing the node on the end, you can add nodes to the end and move forward. 
+ Maybe instead of actually removing the node on the end, you can add nodes to the end and move the head forward. 
 
  If we point end.next to start, move the head to start.next, then set start.next to null, that should work. Now start.next = start and end.next = end
 
- This is going to work, but there is one wrinkle. The number of rotations of this kind is not k, it is size - k.     
+ This type of rotation is possible, but it is important to note that it is a different kind of rotation than the one we defined earlier!
+ 
+ type 1, moving the last node to the front:
 
+ [0, 1, 2] -> [2, 0, 1]
+
+ type 2. moving the first node to the end
+
+ [0, 1, 2] -> [1, 2, 0]
+ [1, 2, 0] -> [2, 0, 1]
+ 
+ We can get the same result, but it takes size - k rotations, not k rotations, since we are effectively rotating the opposite direction. 
+ 
  Problem with this is that if k > size, the number of rotations is negative. 
 
- Realizing that rotating the list fully (size times) produces the original list, we can infer that the number of rotations can be defined as size - (size % k)
+ Realizing that rotating the list fully produces the original list, we can infer that the number of rotations can be defined as size - (size % k).
 
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-
         if (head == null) {
             return head;
         }
@@ -49,14 +59,12 @@ class Solution {
         }        
 
         // now we have the head and the tail. Let's rotate!
-
         for(int i = 0; i < size - (k % size); i++) {
             tail.next = head;
             head = head.next;
             tail = tail.next;
             tail.next = null;
         }
-
         return head;
     }
 }
